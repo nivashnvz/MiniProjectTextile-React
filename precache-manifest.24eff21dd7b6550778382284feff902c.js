8ebe0164e33524211b6ed4d6b23cd4060c93753b
nivashnvz <63652818+nivashnvz@users.noreply.github.com>
2021-06-04 18:26:41 +0530

    Add files via upload

---
+++ precache-manifest.24eff21dd7b6550778382284feff902c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd1132e8fee11f2516cad1a80fe530d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4931a49086503561c114",
+    "url": "/static/css/main.312d7a54.chunk.css"
+  },
+  {
+    "revision": "f71c6e5fc60fe5266f4c",
+    "url": "/static/js/2.b260b5c0.chunk.js"
+  },
+  {
+    "revision": "890fe60e51f18d9e8e0004117fd20dc3",
+    "url": "/static/js/2.b260b5c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4931a49086503561c114",
+    "url": "/static/js/main.07c2bf92.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
